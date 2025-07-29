7a3e6043c1f5bf1a70199af8ba5bff771f628d30
Bartek <bartosz.papinski00@gmail.com>
2025-07-29 16:18:06 +0200

    project structure improvement

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     # user apps
 
-    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +136,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        # Dodasz JWT później
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
