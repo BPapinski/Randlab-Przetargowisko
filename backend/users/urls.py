aed65fcee09e6c1afca5c824d43b84301f81e8e5
Bartek <bartosz.papinski00@gmail.com>
2025-07-30 12:43:50 +0200

    authorization via token

---
+++ backend/users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+from users.views import LogoutView
+
+urlpatterns = [
+     path('logout/', LogoutView.as_view(), name='auth_logout'),
+]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist() # Dodaje token do blacklisty
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except KeyError:
+            return Response({"detail": "Brak tokena odświeżającego."}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
